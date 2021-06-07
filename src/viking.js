22aeb79e2be3a01c14f8e5ba22d5ad4f82d94f3d
GianLucas <gianspf@gmail.com>
2021-06-08 01:21:23 +0200

    GianLucas - VikingGame - 08/06

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,73 @@
 // Soldier
-class Soldier {}
+class Soldier {
+constructor(health , strength){
+    this.health = health
+    this.strength = strength
+  }
+  attack(){
+    return this.strength
+  }
+  receiveDamage(dano){
+     this.health -= dano
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+  constructor(name, health, strength){
+  super(health, strength)
+      this.name = name
+      this.health = health
+      this.strength = strength
+  }
+  receiveDamage(dano){
+      let removeDa = this.health -= dano
+      
+      if(removeDa > 0){
+        let vikingAlive = `${this.name} has received ${dano} points of damage`
+
+          return vikingAlive
+      }else{
+        let vikingDies = `${this.name} has died in act of combat`
+
+            return vikingDies
+      }
+  }
+  battleCry(){
+    let odin = "Odin Owns You All!"
+
+      return odin
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+
+receiveDamage(dano){
+    let removeDa = this.health -= dano
+    
+    
+    if(removeDa > 0){
+      let saxonAlive = `A Saxon has received ${removeDa} points of damage`
+
+      return saxonAlive
+
+    }else{
+      let saxonDies = `A Saxon has died in combat`
+      
+      return saxonDies
+
+
+    }
+
+
+
+}
+
+
+
+}
 
 // War
 class War {}
